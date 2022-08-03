3977a509363c48bbca51777088de8a5749e650c5
slavanya <slavanya@C02D3JVAMD6M.local>
2022-08-03 13:28:09 +0530

    Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
   enum :usertype, { Recruiter: 0, Candidate: 1 }   
   has_many :appliedjobs,dependent: :destroy 
-  has_many :jobs , through: :appliedjobs,dependent: :destroy 
+  has_many :jobs ,dependent: :destroy 
  
 
 
--- lib/test_cron.rb
+++ lib/test_cron.rb
@@ -1,5 +1,5 @@
-class TestCron
-    def method1
-        puts "======Cron executed====="
-    end   
-end
+# class TestCron
+#     def method1
+#         puts "======Cron executed====="
+#     end   
+# end
